15e0270d82b14062bcfc07b363bf593305724aed
md.omorFaruksoft90 <mdomorfaruk90g1@gmail.com>
2025-10-11 14:15:25 +0600

    use one object method on another object

---
+++ callAndAction2Bind.js
@@ -0,0 +1,29 @@
+const normalPerson = {
+    firstName: 'Rahim',
+    lastname: 'Uddin',
+    salary: 15000,
+    getFullName: function (){
+        console.log(this.firstName, this.lastname);
+    },
+    chargeBill: function (amount){
+        console.log(this);
+        this.salary = this.salary - amount;
+        return this.salary;
+    }
+}
+
+const heroPerson = {
+    firstName: 'Hero',
+    lastName: 'Balam',
+    salary: 25000,
+}
+
+// normalPerson.chargeBill(120);
+/**
+ * .bind() ==> we can call one obj method on another object. param is the new object. this method return a function that bind with new object
+ */
+const heroChargeBill = normalPerson.chargeBill.bind(heroPerson);
+heroChargeBill(2000);
+heroChargeBill(3000);
+console.log(heroPerson.salary);
+console.log(normalPerson.salary)
